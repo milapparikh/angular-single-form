a68d62c462f4af3e1cad11e01ef870c7adaa9e25
milap-virtueinfo <milap.virtueinfo@gmail.com>
2018-06-10 13:39:06 +0530

    Desc: simple forms validations

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+// app.component.ts
+
+import { Component } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Simple form validation';
+   angForm: FormGroup;
+   constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+   createForm() {
+    this.angForm = this.fb.group({
+       name: ['', Validators.required ],
+       email: ['', Validators.required ]
+       phone: ['', Validators.required ]
+    });
+  }
+}
